e3f6b7f96c1ffb6b8add601b84f500f3e5656257
Grayson Hicks <graysonhicks@gmail.com>
2017-09-19 20:26:21 -0400

    prod and dev config for uglify

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        }, {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    }
+};
