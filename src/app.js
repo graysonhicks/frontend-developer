f302f70b5d6a98f11c3a5005afeb8dd9821e65af
Grayson Hicks <graysonhicks@gmail.com>
2017-09-19 23:12:08 -0400

    basic styles and data, ghpages install

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import JobData from "./data";
-
+import JobData from "./data/index";
 console.log(JobData);
+import Styles from "./styles/styles.scss";
 
 class HelloMessage extends React.Component {
   render() {
-    return <div>Hello {this.props.name}!</div>;
+    return <div className="intro">Hello {this.props.JobData.headline}!</div>;
   }
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Grayson" />, mountNode);
+ReactDOM.render(<HelloMessage JobData={JobData} />, mountNode);
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,3 @@
-
 const webpack = require('webpack');
 const path = require('path');
 
@@ -8,16 +7,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: 'babel-loader'
-        }, {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        }
-      ]
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }, {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
     }
-};
+}
