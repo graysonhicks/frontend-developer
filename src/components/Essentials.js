0792ca5e02615091d61370b1f18580a37010ca01
Grayson Hicks <graysonhicks@gmail.com>
2017-09-23 11:23:54 -0500

    essentials styles and tables

--- src/components/Essentials.js
+++ src/components/Essentials.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
+import { Table } from 'react-bootstrap';
 import {IoCheckmarkCircled, IoThumbsDown} from "react-icons/lib/io";
+import moment from "moment";
 
 class Essentials extends Component {
   buildData() {
@@ -28,53 +30,72 @@ class Essentials extends Component {
     return (
       <div className="essentials">
         <div className="box-row">
-          <div className="box locations">
-            <div className="box-heading">Locations</div>
-            <div>{cleanData.locations}</div>
+          <div className="box company-size table-box">
+              <Table responsive>
+                <thead>
+                  <tr><th>Company Size</th><th></th></tr>
+                </thead>
+                <tbody>
+                  {dataChoices.CompanySize.map((tool, index) => {
+                      if(cleanData.companysize.indexOf(tool) > -1){
+                        return <tr key={index}><td>{tool}</td><td><IoCheckmarkCircled/></td></tr>
+                      } else {
+                        return <tr key={index}><td>{tool}</td><td></td></tr>
+                      }
+                    })}
+                </tbody>
+              </Table>
           </div>
-          <div className="box startdate">
-            <div className="box-heading">Start Date</div>
-            <div>{cleanData.startdate}</div>
-          </div>
+            <div className="box experience table-box">
+              <Table responsive>
+                <thead>
+                  <tr><th>Experience Levels</th><th></th></tr>
+                </thead>
+                <tbody>
+                  {dataChoices.ExperienceLevels.map((tool, index) => {
+                      if(cleanData.experience.indexOf(tool) > -1){
+                        return <tr key={index}><td>{tool}</td><td><IoCheckmarkCircled/></td></tr>
+                      } else {
+                        return <tr key={index}><td>{tool}</td><td></td></tr>
+                      }
+                    })}
+                </tbody>
+              </Table>
+            </div>
+
         </div>
         <div className="box-row">
           <div className="box team-size">
             <div className="box-heading">Team Size</div>
-            <div>{cleanData.teamsize.min} to {cleanData.teamsize.max}</div>
+            <div className="box-description">{cleanData.teamsize.min} to {cleanData.teamsize.max} <br/></div>
           </div>
-          <div className="box experience">
-            <div className="box-heading">Experience</div>
-            {dataChoices.ExperienceLevels.map((tool, index) => {
-                if(cleanData.experience.indexOf(tool) > -1){
-                  return <div key={index}>{tool} <IoCheckmarkCircled/></div>
-                } else {
-                  return <div key={index}>{tool}</div>
-                }
-              })}
-          </div>
-        </div>
-        <div className="box-row">
-          <div className="box employment-type">
-            <div className="box-heading">Employment Type</div>
-            {dataChoices.EmploymentType.map((tool, index) => {
-              if(tool.replace(/\s/g, "") == cleanData.employment){
-                return <div key={index}>{tool} <IoCheckmarkCircled/></div>
-              } else {
-                return <div key={index}>{tool}</div>
-              }
-            })}
+          <div className="box startdate">
+            <div className="box-heading">Start Date</div>
+            <div className="box-description">{moment(cleanData.startdate).format('MMMM Do YYYY')}</div>
           </div>
 
-          <div className="box company-size">
-            <div className="box-heading">Company Size</div>
-            {dataChoices.CompanySize.map((tool, index) => {
-              if(cleanData.companysize.indexOf(tool) > -1){
-                return (<div key={index}>{tool} <IoCheckmarkCircled/></div>)
-              } else {
-                return <div key={index}>{tool}</div>
-              }
-            })}
+        </div>
+        <div className="box-row">
+          <div className="box employment-type table-box">
+              <Table responsive>
+                <thead>
+                  <tr><th>Employment Type</th><th></th></tr>
+                </thead>
+                <tbody>
+                  {dataChoices.EmploymentType.map((tool, index) => {
+                    if(tool.replace(/\s/g, "") == cleanData.employment){
+                      return <tr key={index}><td>{tool}</td><td><IoCheckmarkCircled/></td></tr>
+                    } else {
+                      return <tr key={index}><td>{tool}</td><td></td></tr>
+                    }
+                  })}
+                </tbody>
+              </Table>
           </div>
+            <div className="box locations">
+              <div className="box-heading">Locations</div>
+              <div>{cleanData.locations}</div>
+            </div>
         </div>
       </div>
     )
