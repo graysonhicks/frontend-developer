6804f1ba8c14ef4ab02993fc9120bd9dc66db043
Grayson Hicks <graysonhicks@gmail.com>
2017-09-23 12:32:12 -0500

    some styling for methodology

--- src/components/Methodology.js
+++ src/components/Methodology.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {IoCheckmarkCircled, IoThumbsDown} from "react-icons/lib/io";
+import { Table } from 'react-bootstrap';
 
 class Methodology extends Component {
   buildData() {
@@ -31,79 +32,98 @@ class Methodology extends Component {
     return (
     <div className="methodology">
       <div className="box-row">
-        <div className="box">
-          <div className="box-heading">Static Code Analysis</div>
-          <div className="methodology-descriptions">
-            {dataChoices.CodeAnalysisTools.map((tool, index) => {
-              if(tool.replace(/\s/g, "") == cleanData.staticcodeanalysis){
-                return <div key={index}>{tool} <IoCheckmarkCircled/></div>
-              } else {
-                return <div key={index}>{tool}</div>
-              }
-
-            })}
-          </div>
-        </div>
-        <div className="box flex-2 pigment-indigo">
-          <div className="box-heading">Issue Tracker</div>
-          <div className="methodology-descriptions">
-                {dataChoices.IssueTrackers.map((tool, index) => {
-                    if(tool.replace(/\s/g, "") == cleanData.issuetracker){
-                      return <div key={index}>{tool} <IoCheckmarkCircled/></div>
+          <div className="box code-analysis table-box">
+              <Table responsive>
+                <thead>
+                  <tr><th>Static Code Analysis</th><th></th></tr>
+                </thead>
+                <tbody>
+                  {dataChoices.CodeAnalysisTools.map((tool, index) => {
+                    if(tool.replace(/\s/g, "") == cleanData.staticcodeanalysis){
+                      return <tr key={index}><td>{tool}</td><td><IoCheckmarkCircled/></td></tr>
                     } else {
-                      return <div key={index}>{tool}</div>
+                      return <tr key={index}><td>{tool}</td><td></td></tr>
                     }
-
-                })}
+                  })}
+                </tbody>
+              </Table>
           </div>
-        </div>
-        <div className="box flex-3">
-          <div className="box-heading">Build Server</div>
-          <div className="methodology-descriptions">
+          <div className="box issue-tracker table-box">
+              <Table responsive>
+                <thead>
+                  <tr><th>Issue Tracker</th><th></th></tr>
+                </thead>
+                <tbody>
+                    {dataChoices.IssueTrackers.map((tool, index) => {
+                        if(tool.replace(/\s/g, "") == cleanData.issuetracker){
+                          return <tr key={index}><td>{tool}</td><td><IoCheckmarkCircled/></td></tr>
+                        } else {
+                          return <tr key={index}><td>{tool}</td><td></td></tr>
+                        }
 
-                {dataChoices.BuildServers.map((tool, index) => {
-                  if(tool.replace(/\s/g, "") == cleanData.buildserver){
-                    return <div key={index}>{tool} <IoCheckmarkCircled/></div>
-                  } else {
-                    return <div key={index}>{tool}</div>
-                  }
-
-                })}
+                    })}
+                </tbody>
+              </Table>
           </div>
-        </div>
-        <div className="box">
-          <div className="box-heading">Version Control</div>
-          <div className="methodology-descriptions">
-                {dataChoices.VersionControlSystem.map((tool, index) => {
-                  if(tool.replace(/\s/g, "") == cleanData.versioncontrol){
-                    return <div key={index}>{tool} <IoCheckmarkCircled/></div>
-                  } else {
-                    return <div key={index}>{tool}</div>
-                  }
+          <div className="box build-servers table-box">
+              <Table responsive>
+                <thead>
+                  <tr><th>Build Servers</th><th></th></tr>
+                </thead>
+                <tbody>
+                    {dataChoices.BuildServers.map((tool, index) => {
+                        if(tool.replace(/\s/g, "") == cleanData.buildserver){
+                          return <tr key={index}><td>{tool}</td><td><IoCheckmarkCircled/></td></tr>
+                        } else {
+                          return <tr key={index}><td>{tool}</td><td></td></tr>
+                        }
 
-                })}
+                    })}
+                </tbody>
+              </Table>
           </div>
-        </div>
+          <div className="box version-control table-box">
+              <Table responsive>
+                <thead>
+                  <tr><th>Version Control</th><th></th></tr>
+                </thead>
+                <tbody>
+                    {dataChoices.VersionControlSystem.map((tool, index) => {
+                        if(tool.replace(/\s/g, "") == cleanData.versioncontrol){
+                          return <tr key={index}><td>{tool}</td><td><IoCheckmarkCircled/></td></tr>
+                        } else {
+                          return <tr key={index}><td>{tool}</td><td></td></tr>
+                        }
+
+                    })}
+                </tbody>
+              </Table>
+          </div>
       </div>
       <div className="box-row">
+
             <div className="box checkbox-container">
-              <div className="box-heading ">Code Reviews {cleanData.codereviews ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+              <div className="daytoday">
+                Day<br/>to<br/>Day
+              </div>
+              <div className="checkboxes">
+                            <div className="box-heading ">Code Reviews {cleanData.codereviews ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
 
-              <div className="box-heading ">Commit On Day One {cleanData.commitondayone ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+                            <div className="box-heading ">Commit On Day One {cleanData.commitondayone ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
 
-              <div className="box-heading ">Fail Fast {cleanData.failfast ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+                            <div className="box-heading ">Fail Fast {cleanData.failfast ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
 
-              <div className="box-heading ">Standups   {cleanData.standups ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+                            <div className="box-heading ">Standups   {cleanData.standups ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
 
-            </div>
-            <div className="box checkbox-container french-pass">
-              <div className="box-heading ">Integration Tests {cleanData.integrationtests ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+                            <div className="box-heading ">Integration Tests {cleanData.integrationtests ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
 
-              <div className="box-heading ">Prototyping {cleanData.prototyping ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+                            <div className="box-heading ">Prototyping {cleanData.prototyping ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
 
-              <div className="box-heading ">Quick Start {cleanData.quickstart ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+                            <div className="box-heading ">Quick Start {cleanData.quickstart ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
 
-              <div className="box-heading ">Unit Tests {cleanData.unittests ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+                            <div className="box-heading ">Unit Tests {cleanData.unittests ? (<IoCheckmarkCircled className="methodology-checks"/>) : <IoThumbsDown/>}</div>
+              </div>
+
 
             </div>
         </div>
