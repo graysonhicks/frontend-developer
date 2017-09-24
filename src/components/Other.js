1b60f5e19cb469b01c4656e339c6b8542b971bbf
Grayson Hicks <graysonhicks@gmail.com>
2017-09-23 23:42:59 -0500

    other component styles

--- src/components/Other.js
+++ src/components/Other.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-import {IoCheckmarkCircled, IoThumbsDown} from "react-icons/lib/io";
+import {IoCheckmarkCircled, IoThumbsDown, IoArrowDownA, IoAt} from "react-icons/lib/io";
 
 class Other extends Component {
   buildOthers(){
@@ -8,18 +8,20 @@ class Other extends Component {
     let i = 1;
       for (var k in this.props.jobData) {
         if (this.props.jobData.hasOwnProperty(k)) {
-          others.push(<div key={i}>{this.props.jobData[k]}</div>)
+          others.push(<div className="others-text" key={i}>{this.props.jobData[k]}</div>)
         }
         i++;
       }
-      console.log(others);
       return others;
   }
   render() {
-console.log(this.props);
+
     return (
     <div className="other">
+      <div className="other-heading">Here's the bottom line <IoArrowDownA/></div>
       {this.buildOthers()}
+      <div className=""><IoAt /></div>
+      <h1 className="start-screen-heading">Gridium</h1>
     </div>
     );
   }}
