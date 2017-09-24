32b0bb2d65496a75b7110cd0483212f9845b430f
Grayson Hicks <graysonhicks@gmail.com>
2017-09-24 15:40:49 -0400

    404 page

--- src/components/Oops.js
+++ src/components/Oops.js
@@ -1,15 +1,23 @@
 import React, {Component} from "react";
 import HomeLink from "./HomeLink";
-
+import ParticlesDisplay from "./ParticlesDisplay"
+import Fade from "./Fade";
 class Oops extends Component {
   render() {
     return (
       <div className="main-container">
-        <div className="oops">
+        <Fade>
+                  <div className="oops">
 
-          <div className="oops-warning">Sorry! Only so many webpages to go around!</div>
-          <div className="oops-text">Click here to go back home: <HomeLink/></div>
-        </div>
+                    <div className="oops-warning">Sorry! Only so many webpages to go around!</div>
+                    <div className="oops-text">Click here to go back home: <HomeLink/></div>
+                    <div className="oops-particles-container">
+                        <ParticlesDisplay />
+                    </div>
+
+                  </div>
+        </Fade>
+
       </div>
 
     )
